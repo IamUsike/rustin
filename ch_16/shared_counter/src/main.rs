af4b2d55cfb14d3cc48448a249f37d7d466ee726
IamUsike <sathwikthanmay4@gmail.com>
2026-08-16 13:03:10 +0530

    rejection makes a man stronger

---
+++ ch_16/shared_counter/src/main.rs
@@ -0,0 +1,7 @@
+//I wont write the program with normal i and refcell cos ik rc and
+//refcell dont implement the send or sync traits so they aren't safe
+//to send over threads.
+
+fn main() {
+    println!("Hello, world!");
+}
