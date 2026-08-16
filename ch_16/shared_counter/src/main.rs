d0ddf41db6802fb884ece78034c989f8a3ef0936
IamUsike <sathwikthanmay4@gmail.com>
2026-08-16 14:33:41 +0530

    shared ctr
    hit the earth like an asteroid /\n did nothing but shoot for the moon since (pew)

--- ch_16/shared_counter/src/main.rs
+++ ch_16/shared_counter/src/main.rs
@@ -1,7 +1,32 @@
-//I wont write the program with normal i and refcell cos ik rc and
+//I wont write the program with normal i32 cos all the threads need to
+//own the value. Rc  and refcell cos ik rc and
 //refcell dont implement the send or sync traits so they aren't safe
 //to send over threads.
 
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let ctr = Arc::new(Mutex::new(0));
+    let mut handles = Vec::new();
+
+    //spawn 5 threads
+    for _ in 0..5 {
+        let ctr = Arc::clone(&ctr);
+        let handle = thread::spawn(move || {
+            //each thread increments the counter 1k times
+            for _ in 0..1000 {
+                let mut num = ctr.lock().unwrap();
+                *num += 1;
+            }
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("The final result is: {}", *ctr.lock().unwrap());
 }
